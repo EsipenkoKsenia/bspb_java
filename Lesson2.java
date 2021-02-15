64cfa4845c188d5fc786abf7076e7befccbb15d6
EsipenkoKsenia <k.esipenko@lipt-soft.ru>
2021-02-15 18:39:48 +0300

    add Lesson2

--- Lesson2.java
+++ Lesson2.java
@@ -1,41 +1,47 @@
-//Задание: создать массив данных, содержащий данные о клиентах (ФИО/возраст/пол) - 10 шт.
+import java.util.Arrays;
+import java.util.Comparator;
+
+// Задание: создать массив данных, содержащий данные о клиентах (ФИО/возраст/пол) - 10 шт.
 // Информацию о клиентах указать в коде.
-// Распечатать на экран данные всех клиентов мужского пола, отсортированных по возрасту
+// Распечатать на экран данные всех клиентов пол = "муж", отсортированных по возрастанию возраста
 public class Lesson2 {
     public static void main(String[] args) {
-        class Employee{
-            public int    employeeId;
+
+        class Clients{
+            public int clientId;
             public String name;
             public int age;
             public String gender;
-            public Employee(int employeeId, String name, int age, String gender){
-                this.employeeId = employeeId;
+            public Clients(int clientId, String name, int age, String gender){
+                this.clientId = clientId;
                 this.name = name;
                 this.age = age;
                 this.gender = gender;
             }
         }
-//
-        Employee[] employeeArray = new Employee[10];
-        employeeArray[0] = new Employee(1, "Иванова Татьяна Васильевна",18, "жен");
-        employeeArray[1] = new Employee(2,"Петров Игорь Дмитриевич", 64,"муж");
-        employeeArray[2] = new Employee(3,"Киселев Геннадий Александрович", 28, "муж"  );
-        employeeArray[3] = new Employee(4,"Алексеев Кирилл Афанасьевич", 81, "муж"  );
-        employeeArray[4] = new Employee(5,"Васильева Екатерина Анатольевна", 45, "жен"  );
-        employeeArray[5] = new Employee(6,"Федорова Тамара Алексеевна", 33, "жен"  );
-        employeeArray[6] = new Employee(7,"Озерова Людмила Аркадьевна", 57, "жен"  );
-        employeeArray[7] = new Employee(8,"Константинов Роман Игоревич", 66, "муж"  );
-        employeeArray[8] = new Employee(9,"Фролова Татьяна Михайловна", 25, "жен"  );
-        employeeArray[9] = new Employee(10,"Васнецов Архип Юрьевич", 29, "муж"  );
-//
-//        java.util.Arrays.sort(employeeArray, new Comparator() {
-//            @Override
-//            public int compare(Employee e1, Employee e2) {
-//                return e1.employeeId - e2.employeeId;
-//            }
-//        });
-        for(int i=0; i < employeeArray.length; i++) {
-            System.out.println("ID = " + employeeArray[i].name+employeeArray[i].gender);
+
+        Clients[] clientsArray = new Clients[10];
+        clientsArray[0] = new Clients(1, "Иванова Татьяна Васильевна",18, "жен");
+        clientsArray[1] = new Clients(2,"Петров Игорь Дмитриевич", 64,"муж");
+        clientsArray[2] = new Clients(3,"Киселев Геннадий Александрович", 28, "муж"  );
+        clientsArray[3] = new Clients(4,"Алексеев Кирилл Афанасьевич", 81, "муж"  );
+        clientsArray[4] = new Clients(5,"Васильева Екатерина Анатольевна", 45, "жен"  );
+        clientsArray[5] = new Clients(6,"Федорова Тамара Алексеевна", 33, "жен"  );
+        clientsArray[6] = new Clients(7,"Озерова Людмила Аркадьевна", 57, "жен"  );
+        clientsArray[7] = new Clients(8,"Константинов Роман Игоревич", 66, "муж"  );
+        clientsArray[8] = new Clients(9,"Фролова Татьяна Михайловна", 25, "жен"  );
+        clientsArray[9] = new Clients(10,"Васнецов Архип Юрьевич", 29, "муж"  );
+
+        Arrays.sort(clientsArray, new Comparator<Clients>() {
+            @Override
+            public int compare(Clients c1,Clients c2) {
+                return c1.age-c2.age;
+            }
+        });
+
+        for(int i=0; i < clientsArray.length; i++) {
+            if(clientsArray[i].gender == "муж")
+            System.out.println("ID-" + clientsArray[i].clientId + " ФИО:" + clientsArray[i].name + " Возраст-" + clientsArray[i].age + " Пол-" + clientsArray[i].gender);
         }
     }
 }
